3ca74690bfaf99b43bf0acba7e67c262606125c1
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-06-28 21:32:27 +0700

    Update retrieve_supplier.php

--- php_apobase/retrieve_supplier.php
+++ php_apobase/retrieve_supplier.php
@@ -1,28 +1,30 @@
 <?php
-require("koneksi.php");
-$perintah = "SELECT * FROM supplier";
-$eksekusi = mysqli_query($conn, $perintah);
-$cek = mysqli_affected_rows($conn);
+    require("koneksi.php");
+          $perintah = "SELECT * FROM supplier";
+          $eksekusi = mysqli_query($conn, $perintah);
+          $cek = mysqli_affected_rows($conn);
 
-if ($cek > 0){
-    $response["kode"] = 1;
-    $response["pesan"] = "Data Tersedia";
-    $response["data"] = array();
+    if ($cek > 0)
+    {
+        $response["kode"] = 1;
+        $response["pesan"] = "Data Tersedia";
+        $response["data"] = array();
 
-    while($ambil = mysqli_fetch_object($eksekusi)){
-        $F['supplier_id'] = $ambil->supplier_id;
-        $F['name'] = $ambil->name;
-        $F['phone'] = $ambil->phone;
-        $F['address'] = $ambil->address;
-        $F['description'] = $ambil->description;
+        while($ambil = mysqli_fetch_object($eksekusi)){
+            $F['supplier_id'] = $ambil->supplier_id;
+            $F['name'] = $ambil->name;
+            $F['phone'] = $ambil->phone;
+            $F['address'] = $ambil->address;
+            $F['description'] = $ambil->description;
 
-        array_push($response["data"], $F);
+            array_push($response["data"], $F);
+        }
     }
-}
-else{
-    $response["kode"] = 0;
-    $response["pesan"] = "Data Tidak Tersedia";
-}
-echo json_encode($response);
-mysqli_close($conn);
+    else
+    {
+        $response["kode"] = 0;
+        $response["pesan"] = "Data Tidak Tersedia";
+    }
+    echo json_encode($response);
+    mysqli_close($conn);
 ?>
