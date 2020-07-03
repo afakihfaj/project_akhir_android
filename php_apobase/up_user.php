185cff4ffc030d250e2f08f683c25d457822cce9
afakihfaj <55530033+afakihfaj@users.noreply.github.com>
2020-07-03 20:21:36 +0700

    Update up_user.php

--- php_apobase/up_user.php
+++ php_apobase/up_user.php
@@ -8,16 +8,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST["username"];
     $name = $_POST["name"];
     $address = $_POST["address"];
-    $perintah = "UPDATE user SET username ='$username',name ='$name',address ='$address', WHERE user_id='$user_id'";
+    $perintah = "UPDATE user SET username ='$username',name ='$name',address ='$address' WHERE user_id='$user_id'";
     $eksekusi = mysqli_query($conn, $perintah);
     $cek = mysqli_affected_rows($conn);
 
     if ($cek > 0) {
         $response["kode"] = 1;
-        $response["pesan"] = "Data berhasil dihapus";
+        $response["pesan"] = "Data berhasil diupdate";
     } else {
         $response["kode"] = 0;
-        $response["pesan"] = "Data gagal dihapus";
+        $response["pesan"] = "Data gagal diupdate";
     }
 } else {
     $response["kode"] = 0;
